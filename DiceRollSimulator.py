5ab19fe8e2c5e6511a71514f4a78ab1cbaa5aa5e
ermax775 <53866227+ermax775@users.noreply.github.com>
2021-08-01 17:12:27 +0300

    Add files via upload
    A simple Dice Rolling Simulator with if else and while loop in python.

---
+++ DiceRollSimulator.py
@@ -0,0 +1,48 @@
+import random
+print("This is a dice stimulator")
+x = "y"
+
+print("Here is your luck in first dice roll")
+while x == "y" or x == "Y":
+    number = random.randint(1,6)
+
+    if number == 1:
+        print("----------")
+        print("|        |")
+        print("|    O   |")
+        print("|        |")
+        print("----------")
+    if number == 2:
+        print("----------")
+        print("|        |")
+        print("| O    O |")
+        print("|        |")
+        print("----------")
+    if number == 3:
+        print("----------")
+        print("|    O   |")
+        print("|    O   |")
+        print("|    O   |")
+        print("----------")
+    if number == 4:
+        print("----------")
+        print("| O    O |")
+        print("|        |")
+        print("| O    O |")
+        print("----------")
+    if number == 5:
+        print("----------")
+        print("| O    O |")
+        print("|    O   |")
+        print("| O    O |")
+        print("----------")
+    if number == 6:
+        print("----------")
+        print("| O    O |")
+        print("| O    O |")
+        print("| O    O |")
+        print("----------")
+    x = input("\n Press y or Y to roll again \n")
+print("System is exiting. See you again !")
+
+   
